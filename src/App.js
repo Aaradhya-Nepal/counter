f30f4d89c79fd737505b8875cb1472c4179c0cb5
Aaradhya <104313264+Tiresomee@users.noreply.github.com>
2023-02-02 12:52:44 +0545

    Counter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  function counter() {
+    setCount(count + 1);
+  }
+  function reverseCounter() {
+    setCount(count - 1);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={reverseCounter}>-1</button>
+      <h1>{count}</h1>
+      <button onClick={counter}>+1</button>
+    </>
   );
 }
 
